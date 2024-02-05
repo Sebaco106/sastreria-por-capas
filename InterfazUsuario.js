c90d6b3f05083c24aa54ac280802950665bdb018
Sebaco106 <sebariv2011@hotmail.com>
2024-02-05 15:01:21 -0500

    primera version

---
+++ InterfazUsuario.js
@@ -0,0 +1,51 @@
+/*const NegocioSastreria = require('../Capa negocios/NegocioSastreria');
+
+class InterfazUsuario {
+    constructor(negocioSastreria) {
+      this.negocioSastreria = negocioSastreria;
+    }
+  
+    mostrarOrdenes() {
+      const ordenes = this.negocioSastreria.obtenerOrdenes();
+      console.log("Ordenes de Trabajo:");
+      ordenes.forEach((orden, index) => {
+        console.log(`${index + 1}. Tipo Terno: ${orden.tipoTerno}, Color: ${orden.color}, Talla: ${orden.talla}, Precio Total: ${orden.precioTotal}`);
+      });
+    }
+  
+    registrarOrden(tipoTerno, color, talla, precioTotal) {
+      this.negocioSastreria.crearOrden(tipoTerno, color, talla, precioTotal);
+      console.log("Orden registrada con éxito.");
+    }
+  }
+module.exports = InterfazUsuario;*/
+
+class InterfazUsuario {
+  constructor() {}
+
+  async mostrarOrdenes() {
+    try {
+      const ordenes = await this.negocioSastreria.obtenerOrdenes();
+
+      console.log("Ordenes de Trabajo:");
+      ordenes.forEach((orden, index) => {
+        console.log(`${index + 1}. Tipo Terno: ${orden.tipoTerno}, Color: ${orden.color}, Talla: ${orden.talla}, Precio Total: ${orden.precioTotal}`);
+      });
+    } catch (error) {
+      console.error("Error al obtener órdenes:", error.message);
+    }
+  }
+
+  async registrarOrden(tipoTerno, color, talla, precioTotal) {
+    try {
+      await this.negocioSastreria.crearOrden(tipoTerno, color, talla, precioTotal);
+
+      console.log("Orden registrada con éxito.");
+    } catch (error) {
+      console.error("Error al registrar la orden:", error.message);
+    }
+  }
+}
+
+module.exports = InterfazUsuario;
+
